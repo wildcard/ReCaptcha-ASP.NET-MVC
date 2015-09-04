0d05014cec5db5aca079bd83e818a6388e72e0a3
Kobi Kadosh <kobi.kadosh@gmail.com>
2015-09-04 03:36:43 +0300

    update version to fix nuget add test proj - empty set razorgenerator to developmentDependency - won't be included in users project

--- reCaptcha.test/Properties/AssemblyInfo.cs
+++ reCaptcha.test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("reCaptcha")]
-[assembly: AssemblyDescription("Google reCaptcha ASP.NET MVC")]
+[assembly: AssemblyTitle("reCaptcha.test")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kobi Kadosh")]
-[assembly: AssemblyProduct("reCaptcha")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("reCaptcha.test")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d8088720-a1e2-4417-9109-77c21e3f4a95")]
+[assembly: Guid("61dfece9-6e27-4b40-993c-acabd637c0cb")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ reCaptcha.test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using reCaptcha;
+
+namespace reCaptcha.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+        }
+    }
+}
